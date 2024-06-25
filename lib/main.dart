5e91f4f3b44eadba17c9b0fa223cc908cfe6eebd
Suthon <suthon.s@psu.ac.th>
2024-06-25 13:26:59 +0700

    fetch links

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class Link {
+  String title;
+  String imageUrl;
+  String? link;
+  Link({required this.title, required this.imageUrl, this.link});
+
+  factory Link.fromJson(dynamic data) {
+    return Link(
+        title: data['title'], imageUrl: data['imageUrl'], link: data['link']);
+  }
+}
+
+Future<List<Link>> fetchLinks() async {
+  var dio = Dio();
+  var result = await dio.get('https://api.pupasoft.com/links');
+  return result.data.map<Link>(Link.fromJson).toList();
 }
 
 class MainApp extends StatelessWidget {
